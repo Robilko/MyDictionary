1414448e63e2234c4a815502b8d7ff8aeabdb886
Robilko <sasha.robson@gmail.com>
2022-04-19 00:48:22 +0300

    ДЗ 8. Пункт 4. * Добавил RenderEffect в DescriptionActivity.kt, а именно замыливается весь экран, до тех пор пока не подгрузится картинка по полученной ссылке

--- app/src/main/java/com/example/mydictionary/view/descriptionscreen/DescriptionActivity.kt
+++ app/src/main/java/com/example/mydictionary/view/descriptionscreen/DescriptionActivity.kt
@@ -2,7 +2,10 @@ package com.example.mydictionary.view.descriptionscreen
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.RenderEffect
+import android.graphics.Shader
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.ImageView
@@ -149,14 +152,31 @@ class DescriptionActivity : AppCompatActivity() {
         val request = LoadRequest.Builder(this)
             .data("https:$imageLink")
             .target(
-                onStart = {},
-                onSuccess = { result -> imageView.setImageDrawable(result) },
-                onError = { imageView.setImageResource(R.drawable.ic_load_error_vector) }
+                onStart = { renderEffectOn() },
+                onSuccess = { result ->
+                    renderEffectOff()
+                    imageView.setImageDrawable(result) },
+                onError = {
+                    renderEffectOff()
+                    imageView.setImageResource(R.drawable.ic_load_error_vector) }
             )
             //.transformations(CircleCropTransformation()) //метод обрезает изображение по кругу. Можно убрать
             .build()
 
         ImageLoader(this).execute(request)
+    }
+
+    private fun renderEffectOn() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            binding.root.setRenderEffect(RenderEffect.createBlurEffect(
+                15f, 15f, Shader.TileMode.MIRROR
+            ))
+        }
+    }
+    private fun renderEffectOff() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            binding.root.setRenderEffect(null)
+        }
     }
 
     companion object {
